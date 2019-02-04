4867702b8b7705cd064105952e175e975d119491
朱颖鑫 <kakkk@live.com>
2019-02-04 07:48:44 +0000

    改了点东西

--- RandomExtract/About.Designer.cs
+++ RandomExtract/About.Designer.cs
@@ -55,7 +55,7 @@
             this.panel1.Controls.Add(this.imageButton1);
             this.panel1.Location = new System.Drawing.Point(-4, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(339, 47);
+            this.panel1.Size = new System.Drawing.Size(339, 51);
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
@@ -63,9 +63,9 @@
             // 
             this.label1.Font = new System.Drawing.Font("站酷快乐体2016修订版", 15.5F);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Location = new System.Drawing.Point(3, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.Size = new System.Drawing.Size(65, 27);
             this.label1.TabIndex = 1;
             this.label1.Text = "关于";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -76,10 +76,10 @@
             this.imageButton1.DialogResult = System.Windows.Forms.DialogResult.None;
             this.imageButton1.DownImage = global::RandomExtract.Properties.Resources.关闭;
             this.imageButton1.HoverImage = global::RandomExtract.Properties.Resources.关闭__悬停;
-            this.imageButton1.Location = new System.Drawing.Point(302, 12);
+            this.imageButton1.Location = new System.Drawing.Point(302, 13);
             this.imageButton1.Name = "imageButton1";
             this.imageButton1.NormalImage = global::RandomExtract.Properties.Resources.关闭;
-            this.imageButton1.Size = new System.Drawing.Size(24, 23);
+            this.imageButton1.Size = new System.Drawing.Size(24, 25);
             this.imageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageButton1.TabIndex = 0;
             this.imageButton1.TabStop = false;
@@ -91,9 +91,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::RandomExtract.Properties.Resources.头像;
-            this.pictureBox1.Location = new System.Drawing.Point(113, 53);
+            this.pictureBox1.Location = new System.Drawing.Point(113, 57);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(109, 110);
+            this.pictureBox1.Size = new System.Drawing.Size(109, 119);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -102,19 +102,19 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.label2.Location = new System.Drawing.Point(-4, 195);
+            this.label2.Location = new System.Drawing.Point(-4, 211);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(339, 20);
+            this.label2.Size = new System.Drawing.Size(339, 22);
             this.label2.TabIndex = 2;
-            this.label2.Text = string.Format("Version：{0}", Config.Ver);
+            this.label2.Text = "Version：1.1.0.1";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("站酷快乐体2016修订版", 13F);
-            this.label3.Location = new System.Drawing.Point(-4, 175);
+            this.label3.Location = new System.Drawing.Point(-4, 190);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(339, 20);
+            this.label3.Size = new System.Drawing.Size(339, 22);
             this.label3.TabIndex = 3;
             this.label3.Text = "抽号点名器";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,9 +122,9 @@
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.label4.Location = new System.Drawing.Point(-4, 215);
+            this.label4.Location = new System.Drawing.Point(-4, 233);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(339, 20);
+            this.label4.Size = new System.Drawing.Size(339, 22);
             this.label4.TabIndex = 4;
             this.label4.Text = "Author：Kakkk";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -133,9 +133,9 @@
             // 
             this.label5.Font = new System.Drawing.Font("微软雅黑", 11F);
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(0, 235);
+            this.label5.Location = new System.Drawing.Point(0, 255);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.Size = new System.Drawing.Size(93, 22);
             this.label5.TabIndex = 5;
             this.label5.Text = "Github：";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -143,9 +143,9 @@
             // linkLabel1
             // 
             this.linkLabel1.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.linkLabel1.Location = new System.Drawing.Point(79, 235);
+            this.linkLabel1.Location = new System.Drawing.Point(79, 255);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(256, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(256, 22);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/kakkk/RandomExtract";
@@ -155,9 +155,9 @@
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.label6.Location = new System.Drawing.Point(0, 255);
+            this.label6.Location = new System.Drawing.Point(0, 276);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(126, 20);
+            this.label6.Size = new System.Drawing.Size(126, 22);
             this.label6.TabIndex = 7;
             this.label6.Text = "WebSite：";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -165,9 +165,9 @@
             // linkLabel2
             // 
             this.linkLabel2.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.linkLabel2.Location = new System.Drawing.Point(113, 256);
+            this.linkLabel2.Location = new System.Drawing.Point(113, 277);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(222, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(222, 22);
             this.linkLabel2.TabIndex = 8;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "https://kakkk.cc/RandomExtract";
@@ -177,9 +177,9 @@
             // label7
             // 
             this.label7.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.label7.Location = new System.Drawing.Point(0, 279);
+            this.label7.Location = new System.Drawing.Point(0, 302);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(153, 20);
+            this.label7.Size = new System.Drawing.Size(153, 22);
             this.label7.TabIndex = 9;
             this.label7.Text = "Email：";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -188,9 +188,9 @@
             // 
             this.linkLabel3.AutoEllipsis = true;
             this.linkLabel3.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.linkLabel3.Location = new System.Drawing.Point(143, 279);
+            this.linkLabel3.Location = new System.Drawing.Point(143, 302);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(192, 23);
+            this.linkLabel3.Size = new System.Drawing.Size(192, 25);
             this.linkLabel3.TabIndex = 10;
             this.linkLabel3.TabStop = true;
             this.linkLabel3.Text = "kakkk@live.com";
@@ -199,18 +199,18 @@
             // label8
             // 
             this.label8.Font = new System.Drawing.Font("微软雅黑", 11F);
-            this.label8.Location = new System.Drawing.Point(0, 303);
+            this.label8.Location = new System.Drawing.Point(0, 328);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(335, 20);
+            this.label8.Size = new System.Drawing.Size(335, 22);
             this.label8.TabIndex = 11;
-            this.label8.Text = "Copyright ©  2018 By Kakkk";
+            this.label8.Text = "Copyright ©  2018-2019 By Kakkk";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 346);
+            this.ClientSize = new System.Drawing.Size(334, 375);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.label7);
